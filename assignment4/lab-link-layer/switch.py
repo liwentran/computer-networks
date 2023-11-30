a292316a2233498337a6424b3267a704a17c8ff7
Liwen Tran <ltran29@jhu.edu>
2023-11-30 18:21:23 -0500

    [4.2.1] Implemented link layer forwarding/learning and VLANs and trunking

--- assignment4/lab-link-layer/switch.py
+++ assignment4/lab-link-layer/switch.py
@@ -2,15 +2,18 @@
 
 import asyncio
 from cougarnet.sim.host import BaseHost
+import struct 
+import os
+import json
 
 class Switch(BaseHost):
     def __init__(self):
         super().__init__()
-        self._outgoing = {}
-        self._remove_events = {}
-        self._vlans = {}
+        self._outgoing = {}  # MAC : intf
+        self._remove_events = {} 
+        self._vlans = {} 
         self._intfs = {}
-        self._trunks = set()
+        self._trunks = set() # of intf
 
         blob = os.environ.get('COUGARNET_VLAN', '')
         if blob:
@@ -18,12 +21,14 @@ class Switch(BaseHost):
             for myint in self.physical_interfaces:
                 val = vlan_info[myint]
                 if val == 'trunk':
-                    #TODO: Add one line of code to handle this if statement.
-                    
+                    # if intf is trunk, add it to the list of trunk interfaces
+                    self._trunks.add(myint)
                 else:
                     vlan_val = int(val[4:])
                     if vlan_val not in self._vlans:
-                        #TODO: Add one line of code to handle this if statement.
+                        # instantiate for this vlan_val
+                        self._vlans[vlan_val] = []
+                                                
                     self._vlans[vlan_val].append(myint)
                     self._intfs[myint] = vlan_val
         else:
@@ -32,7 +37,10 @@ class Switch(BaseHost):
                 vlan_val = 1
                 if vlan_val not in self._vlans:
                     self._vlans[vlan_val] = []
-                #TODO: Finish filling in the code for the for loop. Hint: it has to do with handling vlan_val and is only 2 lines of code.
+                
+                # Code for the loop. Hint: it has to do with handling vlan_val and is only 2 lines of code.
+                self._vlans[vlan_val].append(myint)
+                self._intfs[myint] = vlan_val
 
         
 
@@ -47,18 +55,20 @@ class Switch(BaseHost):
             vlan = self._intfs[intf]
 
         if dst in self._outgoing:
+            # we know where to send it
             if self._outgoing[dst] in self._trunks:
-                #TODO: complete the following line
-                #frame = 
+                # if the intf is in trunks, then its a 801.1Q frame. Convert to Ethernet frame. 
+                frame = frame[:12] + frame[16:]
 
             self.send_frame(frame, self._outgoing[dst])
         else:
+            # otherwise, broadcast this frame
             for myint in self.physical_interfaces:
                 if intf != myint and \
                         (myint in self._trunks or self._intfs[myint] == vlan):
                     if myint in self._trunks:
-                        #TODO: Complete the following line
-                        #fr = 
+                        # if the intf is in trunks, it must be a 801.1Q frame
+                        fr = frame[:12] + b'\x81\x00' + struct.pack('!H', vlan) + frame[16:]
                     else:
                         fr = frame
 
@@ -69,12 +79,13 @@ class Switch(BaseHost):
         if ev is not None:
             ev.cancel()
         loop = asyncio.get_event_loop()
-        #Complete the following line/
-        #self._remove_events[src] = 
+
+        # The aging time of a new table entry is 8 seconds.
+        # When a frame arrives corresponding to an existing entry, its aging time is reset to 8 seconds.
+        self._remove_events[src] = loop.call_later(8, self.del_outgoing, src)
 
     def del_outgoing(self, src):
-        #TODO: Complete the following line.
-        #del 
+        del self._outgoing[src]
 
 def main():
     Switch().run()
